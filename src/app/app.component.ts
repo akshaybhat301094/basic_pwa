8f6d637ceed722533b46a9a03f36cbf19d710720
arjunbaru <akshaybhat301094@gmail.com>
2019-01-29 19:16:39 +0530

    basic pwa structure with service caching incuded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa';
+  result: any;
+  // update = false;
+
+  constructor(updates: SwUpdate, private dataService: DataService) {
+    updates.available.subscribe(event => {
+        // this.update = true;
+
+        updates.activateUpdate().then(() => {
+          document.location.reload();
+        });
+    });
+  }
+
+  ngOnInit(): void {
+    this.dataService.getJokes().subscribe((res) => {
+      this.result = res;
+    });
+  }
 }
